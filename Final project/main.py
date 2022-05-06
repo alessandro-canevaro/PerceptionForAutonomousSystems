0a4682a815341cc561383aff9f759b0373553fd9
Alessandro Canevaro <s212977@dtu.dk>
2022-05-06 11:33:59 +0200

    3d prediction

--- Final project/main.py
+++ Final project/main.py
@@ -3,6 +3,17 @@ import numpy as np
 import glob
 import matplotlib.pyplot as plt
 
+leftImages = glob.glob('./Stereo_conveyor_without_occlusions/left/*.png')
+rightImages = glob.glob('./Stereo_conveyor_without_occlusions/right/*.png')
+
+rect_map_left_x = np.load(r'matrix_calib_rectify\map_left_x.npy')
+rect_map_left_y = np.load(r'matrix_calib_rectify\map_left_y.npy')
+rect_map_right_x = np.load(r'matrix_calib_rectify\map_right_x.npy')
+rect_map_right_y = np.load(r'matrix_calib_rectify\map_right_y.npy')
+mtx_P_l = np.load(r'matrix_calib_rectify\projection_matrix_left.npy')
+mtx_P_r = np.load(r'matrix_calib_rectify\projection_matrix_right.npy')
+
+
 def findobjectbounds(frame, bgs):
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -53,9 +64,9 @@ def computeDepth(x_center, y_center, left, right, focal_lenght=685, baseline=120
             depth_list.append(focal_lenght*baseline/(disp[y,x]/16.0)) #in mm
 
     result = max(depth_list)
-    if result > 1200:
-        return 0
-    return result
+    if result > 1200 or disp[y,x] < 0:
+        return 0, 0
+    return result, disp[y,x]
 
 def kalmanInit(init_state):
     kalman = cv2.KalmanFilter(6, 3, 0)
@@ -74,55 +85,28 @@ def kalmanInit(init_state):
     kalman.statePre = init_state
     return kalman
 
-def kalmanInit9(init_state):
-    kalman = cv2.KalmanFilter(9, 3, 0)
-    kalman.transitionMatrix = np.array([[1, 1,.5, 0, 0, 0, 0, 0, 0],
-                                        [0, 1, 1, 0, 0, 0, 0, 0, 0],
-                                        [0, 0, 1, 0, 0, 0, 0, 0, 0],
-                                        [0, 0, 0, 1, 1,.5, 0, 0, 0],
-                                        [0, 0, 0, 0, 1, 1, 0, 0, 0],
-                                        [0, 0, 0, 0, 0, 1, 0, 0, 0],
-                                        [0, 0, 0, 0, 0, 0, 1, 1,.5],
-                                        [0, 0, 0, 0, 0, 0, 0, 1, 1],
-                                        [0, 0, 0, 0, 0, 0, 0, 0, 1]], np.float32)
-    kalman.processNoiseCov = 1e-5 * np.eye(9, dtype=np.float32)
-    kalman.measurementNoiseCov = 1e-2 * np.eye(3, dtype=np.float32)
-    kalman.errorCovPost = 1. * np.eye(9, dtype=np.float32)
-    kalman.measurementMatrix = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
-                                         [0, 0, 0, 1, 0, 0, 0, 0, 0],
-                                         [0, 0, 0, 0, 0, 0, 1, 0, 0]], np.float32)
-    kalman.statePre = init_state
-    return kalman
-
 def getMeasure(rect_left, rect_right, backSub):
     founded, center_x, center_y, radius = findobjectbounds(frame, backSub)
     if founded:
-        center_z = computeDepth(int(center_x), int(center_y), rect_left, rect_right)
+        center_z, disp = computeDepth(int(center_x), int(center_y), rect_left, rect_right)
         if center_z == 0:
-            return False, 0, 0, 0, 0
+            return False, 0, 0, 0, 0, 0
         else:
-            return True, center_x, center_y, center_z, radius
-    return False, 0, 0, 0, 0
+            return True, center_x, center_y, center_z, radius, disp
+    return False, 0, 0, 0, 0, 0
 
-rect_map_left_x = np.load(r'matrix_calib_rectify\map_left_x.npy')
-rect_map_left_y = np.load(r'matrix_calib_rectify\map_left_y.npy')
-rect_map_right_x = np.load(r'matrix_calib_rectify\map_right_x.npy')
-rect_map_right_y = np.load(r'matrix_calib_rectify\map_right_y.npy')
-#imgL = cv2.remap(imgL, rect_map_left_x, rect_map_left_y, cv2.INTER_LINEAR)
-#imgR = cv2.remap(imgR, rect_map_right_x, rect_map_right_y, cv2.INTER_LINEAR)
 
-leftImages = glob.glob('./Stereo_conveyor_without_occlusions/left/*.png')
-rightImages = glob.glob('./Stereo_conveyor_without_occlusions/right/*.png')
 frame = cv2.imread(leftImages[0])
 frame = cv2.remap(frame, rect_map_left_x, rect_map_left_y, cv2.INTER_LINEAR)
 
 backSub = cv2.createBackgroundSubtractorMOG2(30, 16, False)
 _ = backSub.apply(frame)
 
-measure_history = [(0, 0, 0)]
-prediction_history = [(0, 0, 0)]
+measure_history = []
+prediction_history = []
 initialized, kalman = False, False
-start, end = 100, 300
+start, end = 100, 500
+save = False
 
 for i, (imgL, imgR) in enumerate(zip(leftImages[start:end], rightImages[start:end])):
     frame = cv2.imread(imgL)
@@ -132,42 +116,43 @@ for i, (imgL, imgR) in enumerate(zip(leftImages[start:end], rightImages[start:en
     frame_right = cv2.imread(imgR)
     rect_right = cv2.remap(frame_right, rect_map_right_x, rect_map_right_y, cv2.INTER_LINEAR)
 
-    founded, center_x, center_y, center_z, radius = getMeasure(rect_left, rect_right, backSub) 
+    founded, center_x, center_y, center_z, radius, disp = getMeasure(rect_left, rect_right, backSub) 
+
+    if save and founded and i%10==0:
+        roi = frame[int(center_y-radius):int(center_y+radius), 
+                    int(center_x-radius):int(center_x+radius)]
+        resized_roi = cv2.resize(roi, (100, 100), interpolation= cv2.INTER_LINEAR)
+        cv2.imwrite(r".\saved_roi\frame_{}.png".format(i), resized_roi)
 
     if founded:
         cv2.circle(frame, (int(center_x), int(center_y)), int(radius), (0, 0, 255), 2) #cirle
         cv2.circle(frame, (int(center_x), int(center_y)), 5, (0, 0, 255), -1) #center
 
+        mp_3d_homogeneous = cv2.triangulatePoints(mtx_P_l, mtx_P_r, np.array([[center_x], [center_y]]), np.array([[center_x-disp], [center_y]]))
+        mp_3d = cv2.transpose(mp_3d_homogeneous)
+        mp_3d = cv2.convertPointsFromHomogeneous(mp_3d).squeeze()
+        #mp_3d = np.multiply(mp_3d, 1000)
+        #print(mp_3d)
+
         if center_x > 1090 and not initialized:
             #"""
-            kalman = kalmanInit(np.array([[center_x], 
-                                          [-2], 
-                                          [center_y], 
-                                          [1], 
-                                          [center_z], 
+            kalman = kalmanInit(np.array([[mp_3d[0]], 
+                                          [0], 
+                                          [mp_3d[1]], 
+                                          [0], 
+                                          [mp_3d[2]], 
                                           [0]], np.float32))
-            """
-            kalman = kalmanInit9(np.array([[center_x], 
-                                [-2],
-                                [0],
-                                [center_y], 
-                                [1], 
-                                [0],
-                                [center_z], 
-                                [0],
-                                [0]], np.float32))
-            """
+
             initialized = True
             cv2.putText(frame,'Filter initialized', (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, 2)
-            #print("initialized", i)
 
         if center_x < 600 and initialized:
             initialized = False
 
         if initialized:
-            kalman.correct(np.array([[center_x],
-                                     [center_y],
-                                     [center_z]], dtype=np.float32))
+            kalman.correct(np.array([[mp_3d[0]],
+                                     [mp_3d[1]],
+                                     [mp_3d[2]]], dtype=np.float32))
 
     if kalman:
         ### Predict the next state
@@ -175,12 +160,21 @@ for i, (imgL, imgR) in enumerate(zip(leftImages[start:end], rightImages[start:en
 
         ### Draw the current tracked state and the predicted state on the image frame ###
         x_pred, y_pred, z_pred = np.matmul(kalman.measurementMatrix, pred).ravel()
-        prediction_history.append((x_pred, y_pred, z_pred))
-        cv2.circle(frame,(int(x_pred), int(y_pred)), 5, (255,0,0),-1)
+
+        point, _ = cv2.projectPoints(np.array([x_pred, y_pred, z_pred]), (0, 0, 0), (0, 0, 0), mtx_P_l[:, 0:-1], None)
+
+        point_x = point[0][0][0]
+        point_y = point[0][0][1]
+        cv2.circle(frame,(int(point_x), int(point_y)), 5, (255,0,0),-1)
+
+        prediction_history.append((x_pred*1000, y_pred*1000, z_pred*1000))
     else:
         prediction_history.append((0, 0, 0))
     
-    measure_history.append((center_x, center_y, center_z))
+    if founded:
+        measure_history.append((mp_3d[0]*1000, mp_3d[1]*1000, mp_3d[2]*1000))
+    else:
+        measure_history.append((0, 0, 0))
 
     text = 'Frame: {}; Prediction: X: {:.1f}; Y: {:.1f}; Z: {:.1f}'.format(i, *prediction_history[-1])
     cv2.putText(frame,text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, 2)
